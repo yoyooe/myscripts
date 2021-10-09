7fdce60c387cc68702d229ac477ccb9a659f29ed
小朋 <2810580330@qq.com>
2021-10-09 23:46:14 +0800

    Create ygyapp.js

---
+++ ygyapp.js
@@ -0,0 +1,11 @@
+/*
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+注册地址：https://yourunsocks.com/#/register
+不含邀请链接
+ygyPhone:手机号#密码
+export ygyPhone='手机号#密码'
+*/
+
+// [task_local]
+//#御果园
+// 10 0 * * * https://raw.fastgit.org/byxiaopeng/myscripts/main/ygyapp.js, tag=御果园APP, enabled=true
